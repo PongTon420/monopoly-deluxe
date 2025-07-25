0c72433ce2381c73cb9687bd3d9dc305926d2f23
Pong Ton <tututien123@gmail.com>
2025-07-25 17:17:44 +0700

    cmt

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+// Supabase wee:
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 const supabaseUrl = 'https://ydembgafgkmhpnydqqyw.supabase.co';
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlkZW1iZ2FmZ2ttaHBueWRxcXl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMzNjQwNTIsImV4cCI6MjA2ODk0MDA1Mn0.QnTKCsDAgM_AhtQGzxfN88qGyyMBKhk5-hmBnqdovoc";
 const supabase = createClient(supabaseUrl, supabaseKey);
+
+//-----------------Ket noi database o tren, ke no di wee-------------------------
 
 const nameInput = document.getElementById('changeName');
 const changeNameButton = document.getElementById('changeNameBttn');
