ba0e9e9e64362b0fe8d7f2b5020889fd209c3303
Pong Ton <tututien123@gmail.com>
2025-07-26 11:37:13 +0700

    add canvas

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// Supabase wee:
+// Supabase:
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const supabaseUrl = 'https://ydembgafgkmhpnydqqyw.supabase.co';
+const supabaseUrl = "https://ydembgafgkmhpnydqqyw.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlkZW1iZ2FmZ2ttaHBueWRxcXl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMzNjQwNTIsImV4cCI6MjA2ODk0MDA1Mn0.QnTKCsDAgM_AhtQGzxfN88qGyyMBKhk5-hmBnqdovoc";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
@@ -106,7 +106,16 @@ function showScreen(id) {
     document.getElementById(id).style.display = 'block';
 };
 
+function SelectRoom()
+{
+    const roomSelectionCanvas = document.getElementById('roomSelectionCanvas');
+    const ctx = roomSelectionCanvas.getContext("2d");
+    roomSelectionCanvas.width = 700;
+    roomSelectionCanvas.height = 500;
+}
+
 //run shit here:
 initPlayer();
 showScreen(showScreenId);
 changeNameButton.addEventListener('click', changeName);
+window.addEventListener('load', SelectRoom);
